1c68ddcaf7de38b9e2e68c40488da0fefc7c0c58
asoukitelk6 <asoukitelk6@gmail.com>
2025-05-29 23:20:32 +0400

    все задачи выполнены и добавлен файл для сохранения животных

--- main.py
+++ main.py
@@ -21,11 +21,105 @@
 (например, `feed_animal()` для `ZooKeeper` и `heal_animal()`
 для `Veterinarian`).
 
-
-
 Дополнительно:
 
 Попробуйте добавить дополнительные функции в вашу программу,
 такие как сохранение информации о зоопарке в файл и возможность
 её загрузки, чтобы у вашего зоопарка было "постоянное состояние"
 между запусками программы.'''
+
+class Animal():
+    def __init__(self,name='животное',age=1):
+        self.name=name
+        self.age=age
+    def make_sound(self):
+        pass
+    def eat(self):
+        pass
+    def __repr__(self):
+        return f'{self.__class__.__name__}:{self.name}:{self.age}'
+
+class Bird(Animal):
+    def make_sound(self):
+        print('карр, чирик и тп')
+    def eat(self):
+        print('Птица ест')
+
+class Mammal(Animal):
+    def make_sound(self):
+        print('мяу,гав и тп')
+    def eat(self):
+        print('Млекопитающее ест')
+
+class Reptile(Animal):
+    def make_sound(self):
+        print('шшшшш')
+    def eat(self):
+        print('Рептилия ест')
+
+def animal_sound(animals):
+    for animal in animals:
+        animal.make_sound()
+
+def eat(animal):
+    animal.eat()
+
+class Zoo():
+    def __init__(self):
+        self.animals=[]
+        self.staff=[]
+        with open('zoo.txt','r',encoding='utf-8') as file:
+            for line in file:
+                parts=line.strip().split(':')
+                if len(parts) == 3:
+                    class_name, name, age =parts
+                    age = int(age)
+                    if class_name == 'Bird':
+                        animal = Bird(name, age)
+                    elif class_name == 'Mammal':
+                        animal = Mammal(name, age)
+                    elif class_name == 'Reptile':
+                        animal = Reptile(name, age)
+                    else:
+                        continue
+                    self.animals.append(animal)
+    def add_animal(self,animal):
+        self.animals.append(animal)
+        with open('zoo.txt', 'w',encoding='utf-8') as file:
+            for animal in self.animals:
+                file.write(repr(animal) + '\n')
+        print(f'В зоопарк добавлено новое животное - {animal.name}')
+    def add_employee(self,employee):
+        self.staff.append(employee)
+        print(f'В зоопарк добавлен новый сотрудник - {employee.__class__.__name__}')
+    def list_animals(self):
+        for animal in self.animals:
+            print(f'{animal.__class__.__name__} - {animal.name}, возраст - {animal.age}')
+
+
+class ZooKeeper(Zoo):
+    def feed_animal(self,animal):
+        print(f'Смотритель кормит {animal.name}')
+
+class Veterinarian(Zoo):
+    def heal_animal(self,animal):
+        print(f'Ветеринар лечит {animal.name}')
+
+dog=Mammal('собака')
+bird=Bird('аист')
+reptile=Reptile('рептилия')
+dog.eat()
+
+zoo1=Zoo()
+keeper=ZooKeeper()
+veterinarian=Veterinarian()
+
+zoo1.add_employee(keeper)
+zoo1.add_employee(veterinarian)
+
+animal_sound(zoo1.animals)
+
+keeper.feed_animal(zoo1.animals[0])
+veterinarian.heal_animal(zoo1.animals[0])
+
+zoo1.list_animals()
